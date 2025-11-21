04504b13222fe3da7ef84ef01d2650e8bcc9aa26
MeAkash77 <itsaakash77@gmail.com>
2025-11-21 10:09:49 +0530

    first commit

--- components/social-links.tsx
+++ components/social-links.tsx
@@ -5,13 +5,13 @@ import { faGithub, faYoutube, faLinkedin } from '@fortawesome/free-brands-svg-ic
 const SocialMediaLinks = () => {
     return (
         <div className='flex flex-row gap-4'>
-            <a href="https://github.com/mrExplorist" target="_blank" rel="noopener noreferrer">
+            <a href="https://github.com/MeAkash77" target="_blank" rel="noopener noreferrer">
                 <FontAwesomeIcon icon={faGithub} size="2x" />
             </a>
             <a href="https://www.youtube.com/" target="_blank" rel="noopener noreferrer">
                 <FontAwesomeIcon icon={faYoutube} size="2x" />
             </a>
-            <a href="https://www.linkedin.com/in/pinglalit/" target="_blank" rel="noopener noreferrer">
+            <a href="https://www.linkedin.com/in/me-akash77" target="_blank" rel="noopener noreferrer">
                 <FontAwesomeIcon icon={faLinkedin} size="2x" />
             </a>
         </div>
